dfe79847d9bfb64b5ad1e39925ab189ee46679a6
Hyun Lee <truetulips@naver.com>
2025-10-19 08:35:19 +0900

    axios 프론트 백엔드 분리 작업 및 조정

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,7 +1,7 @@
-const axios = require('axios');
+import axios from 'axios';
 
-module.exports = axios.create({
-  baseURL: 'https://hwaksee.kr', // ✅ 배포용 도메인으로 변경
+export default axios.create({
+  baseURL: 'http://hwaksee.cafe24app.com/', // ✅ 배포용 도메인으로 변경
   // baseURL: 'http://172.30.1.52:5000', // 또는 네트워크 접근 시 'http://localhost:5000'
   headers: {
     'Content-Type': 'application/json'
--- server/ecosystem.config.js
+++ server/ecosystem.config.js
@@ -5,7 +5,7 @@ module.exports = {
                 env: {
                 NODE_ENV: 'production',
                 PORT: 80,
-                MONGO_URI: 'mongodb+srv://design2hyun_db_user:CfeE0ap8EStfeaQo@cluster0.fzelmvg.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',
+                MONGO_URI: 'mongodb+srv://design2hyun_db_user:CfeE0ap8EStfeaQo@cluster0.fzelmvg.mongodb.net/hwaksee?retryWrites=true&w=majority&appName=Cluster0',
                 JWT_SECRET: 'your_jwt_secret',
                 NAVER_CLIENT_ID: '56pRp0qxTMj5X6A9K6S4cu',
                 NAVER_CLIENT_SECRET: '$2a$04$szNqwgYgPPcNZduboVkGTO',

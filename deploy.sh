b5236e382dd338f3d31e7315e14db49e1f2528d7
Hyun Lee <truetulips@naver.com>
2025-10-19 06:36:45 +0900

    불필요 삭제

--- deploy.sh
+++
@@ -1,5 +0,0 @@
-cd /home/hosting_users/hwaksee
-git pull origin main
-npm install --prefix client
-npm run build --prefix client
-pm2 restart hwaksee-prod
--- start.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-start cmd /k "cd /d D:\creative\react\hwaksee\server && call npm start"
-start cmd /k "cd /d D:\creative\react\hwaksee\client && call npm start"

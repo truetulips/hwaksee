7db960d2dc7d9919a937fe91df0bee097f0c7f38
Hyun Lee <truetulips@naver.com>
2025-10-19 05:21:23 +0900

    로컬 테스트

--- start.bat
+++ start.bat
@@ -1,3 +1,3 @@
 @echo off
-start cmd /k "cd /d D:\creative\react\hwaksee\server && call pm2 start ecosystem.config.js --env development"
+start cmd /k "cd /d D:\creative\react\hwaksee\server && call npm start"
 start cmd /k "cd /d D:\creative\react\hwaksee\client && call npm start"
